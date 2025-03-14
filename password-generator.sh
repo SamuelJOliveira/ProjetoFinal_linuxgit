ff5974f46c41b2ce33843b39ef1df0392a127928
Arthur <arthurronald96@gmail.com>
2025-03-14 15:44:38 -0300

    Feat: versão final

--- password-generator.sh
+++ password-generator.sh
@@ -9,7 +9,7 @@ senha=$(cat /dev/urandom | tr -dc "$CARACTERES" | fold -w 10 | head -n 1)
 # Interpretar os argumentos:
 # (Complete o código com as outras funcionalidades usando o elif)
 if [ "$1" = "-h" ]; then
-  echo -e "Bem vindo ao password-generator! Versão 1.0, (c) 2025, Fulano de Tal,DIMAp, UFRN
+  echo -e "Bem vindo ao password-generator! Versão 1.0, (c) 2025, Arthur Ronald e Samuel Jacome,DIMAp, UFRN
 Uso: ./password-generator.sh [OPÇÕES]:
 
 Opções:
@@ -18,30 +18,26 @@ Opções:
 -c : Limpar as senhas do arquivo passwords.txt
 -h : Exibir esse menu
 
-O comportamento padrão do script é gerar uma senha de 8 caracteres minúsculos."
+O comportamento padrão do script é gerar uma senha de 10 caracteres."
 
 elif [ "$1" = "-p" ]; then
-  cat passord.txt
+  cat password.txt
 
-
+  exit
 elif [ "$1" = "-c" ]; then
 
-   > password.txt 
-
+   > password.txt
+  exit
 fi
 
 # Imprime a senha gerada
 # Adicione aqui também a implementação do código que salva a senha no passwords.txt e cria o arquivo caso ele não exista
 # Dica: lembre-se de utilizar o operador "-e" junto com a condicional "if"!
 echo "Senha gerada: $senha"
-if [ "$senha_gerada" = true ]; then
-	echo "$senha" >> password.txt
+if [ -e "password.txt" ]; then
+	echo "$senha" > password.txt
+
 else
-
-fi
-
-if [ -e "password.txt" = false ]; then 
 	touch "password.txt"
-else
-
+	echo "$senha" > password.txt
 fi
